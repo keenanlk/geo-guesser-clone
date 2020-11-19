ae02fc79be7b0f182d165f205ee6737599969b53
Keenan <keenan@KeenanMacbookPro.local>
2020-11-18 20:39:24 -0600

    user login and registration

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+router.post("/login", authUser);
+
+router.route("/").post(registerUser).get(protect, getUsers);
+
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+
+router
+  .route("/:id")
+  .delete(protect, deleteUser)
+  .get(protect, getUserById)
+  .put(protect, updateUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import path from "path";
 import colors from "colors";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -12,6 +17,15 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/users", userRoutes);
+
+app.use((req, res, next) => {
+  console.log(req);
+});
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
